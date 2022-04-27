0c7a9db4fab6fd6331e7ee6390e9b0ea61201e72
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-04-27 10:10:49 -0300

    Adjustment

--- src/pages/ExploreFoods.jsx
+++ src/pages/ExploreFoods.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function ExploreFoods() {
   return (
-    <h1>Explore Foods Ingredients Page</h1>
+    <h1>Explore Foods Page</h1>
   );
 }
--- src/pages/ExploreFoodsIngredients.jsx
+++ src/pages/ExploreFoodsIngredients.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function ExploreFoodsIngredients() {
   return (
-    <h1>Explore Food Ingredients Page</h1>
+    <h1>Explore Foods Ingredients Page</h1>
   );
 }
