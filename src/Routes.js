2785b8351e6abe2f09788ae7c8a478257b76d091
tadeo <salaviawtadeo@gmail.com>
2022-04-27 01:01:46 +0100

    feat: Login - adiciona tela de login

--- src/Routes.js
+++ src/Routes.js
@@ -29,9 +29,21 @@ const Routes = () => (
     <Route exact path="/explore" component={ Explore } />
     <Route exact path="/explore/foods" component={ ExploreFoods } />
     <Route exact path="/explore/drinks" component={ ExploreDrinks } />
-    <Route exact path="/explore/foods/ingredients" component={ ExploreFoodsIngredients } />
-    <Route exact path="/explore/drinks/ingredients" component={ ExploreDrinksIngredients } />
-    <Route exact path="/explore/foods/nationalities" component={ ExploreFoodsNationalities } />
+    <Route
+      exact
+      path="/explore/foods/ingredients"
+      component={ ExploreFoodsIngredients }
+    />
+    <Route
+      exact
+      path="/explore/drinks/ingredients"
+      component={ ExploreDrinksIngredients }
+    />
+    <Route
+      exact
+      path="/explore/foods/nationalities"
+      component={ ExploreFoodsNationalities }
+    />
     <Route exact path="/profile" component={ Profile } />
     <Route exact path="/done-recipes" component={ DoneRecipes } />
     <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
