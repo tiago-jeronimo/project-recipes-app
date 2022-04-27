60fabd72b2682b8f9fe62caed319c0eed0c986ab
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-04-27 17:57:09 -0300

    Foods Page && getMealByID

---
+++ src/components/MealCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function MealCard({ meal, index }) {
+  return (
+    <Link
+      to={ `/foods/:${meal.idMeal}` }
+      data-testid={ `${index}-recipe-card` }
+    >
+      <img
+        data-testid={ `${index}-card-img` }
+        src={ meal.strMealThumb }
+        alt={ `${meal.strMeal} foto` }
+      />
+      <p data-testid={ `${index}-card-name` }>{meal.strMeal}</p>
+    </Link>
+  );
+}
+
+MealCard.propTypes = {
+  index: PropTypes.string.isRequired,
+  meal: PropTypes.shape({
+    idMeal: PropTypes.string,
+    strMeal: PropTypes.string,
+    strMealThumb: PropTypes.string,
+  }).isRequired,
+};
