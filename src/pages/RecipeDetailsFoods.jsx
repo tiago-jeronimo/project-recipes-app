255545bf9878cecd27ab7ac09e01cbf8d88b126e
Leonardo C <leonardocoel98@gmail.com>
2022-04-28 16:41:39 -0300

    feat: tela de detalhes e ajustes Link Meal Card

--- src/pages/RecipeDetailsFoods.jsx
+++ src/pages/RecipeDetailsFoods.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 export default function RecipeDetailsFoods() {
+  const { id } = useParams();
+  const [meal, setMeal] = useState({});
+  const { strMealThumb, strMeal, strCategory, strInstructions, strYoutube } = meal;
+
+  useEffect(() => {
+    const getMealBy = async (ID) => {
+      const URL_MEAL = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+
+      const resultRequest = await fetch(`${URL_MEAL}${ID}`);
+      const { meals } = await resultRequest.json();
+      setMeal(meals[0]);
+    };
+    getMealBy(id);
+  }, [id]);
+
+  const getIngredients = () => Object.entries(meal).reduce((acc, cur) => {
+    if (cur[0].includes('strIngredient') && cur[1] !== '') acc.push(cur[1]);
+    return acc;
+  }, []);
+
+  const getMeasure = () => Object.entries(meal).reduce((acc, cur) => {
+    if (cur[0].includes('strMeasure') && cur[1] !== ' ') acc.push(cur[1]);
+    return acc;
+  }, []);
+
   return (
-    <h1>Recipe Details Foods Page</h1>
+    <main>
+      <img
+        src={ strMealThumb }
+        alt={ strMeal }
+        data-testid="recipe-photo"
+      />
+      <div>
+        <h1 data-testid="recipe-title">{strMeal}</h1>
+        <img src={ shareIcon } alt="Share Button" data-testid="share-btn" />
+        <img src={ whiteHeartIcon } alt="Favorite Button" data-testid="favorite-btn" />
+      </div>
+      <h3 data-testid="recipe-category">{strCategory}</h3>
+      <h2>Ingredients</h2>
+      {getIngredients().map((ing, i) => (
+        <p
+          key={ ing }
+          data-testid={ `${i}-ingredient-name-and-measure` }
+        >
+          {`- ${ing} - ${getMeasure()[i]} `}
+        </p>
+      ))}
+      <p data-testid="instructions">{strInstructions}</p>
+      <h2>Video</h2>
+      <video src={ strYoutube } data-testid="video"><track kind="captions" /></video>
+      <button type="button" data-testid="start-recipe-btn">Start Recipe</button>
+    </main>
+
   );
 }
