10d6070a1ec6bb2aa9f30c5bdc380f902ded57dc
Beto Ouverney Paz <beto.paz30@gmail.com>
2022-05-03 11:00:42 -0300

    Tela Explore Foods e Drinks

--- src/services/API.jsx
+++ src/services/API.jsx
@@ -1,6 +1,7 @@
 export const endpointsParam = {
   MEALS: {
     all: 'https://www.themealdb.com/api/json/v1/1/list.php?',
+    random: 'https://www.themealdb.com/api/json/v1/1/random.php',
     byIngredient: (ingredient) => `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`,
     byName: (name) => `https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`,
     byLetter: (letter) => `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`,
@@ -10,15 +11,19 @@ export const endpointsParam = {
     byNation: (nation) => `www.themealdb.com/api/json/v1/1/filter.php?a=${nation}`,
   },
   DRINKS: {
+    all: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=',
+    random: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
     byIngredient: (ingredient) => `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`,
     byName: (name) => `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`,
     byLetter: (letter) => `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`,
+    byId: (id) => `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`,
   },
 };
 
 const API = async (type, arg, search) => {
-  console.log(endpointsParam[type][arg](search));
-  const request = await fetch(endpointsParam[type][arg](search));
+  const request = (search)
+    ? await fetch(endpointsParam[type][arg](search))
+    : await fetch(endpointsParam[type][arg]);
   const data = await request.json();
   return data[type.toLowerCase()];
 };
